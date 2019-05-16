14d82d0c59a10b27ca221a0528dd941cb9616bd9
Daniel Gerlag <daniel@gerlag.ca>
2019-05-16 16:37:36 -0700

    initial commit

---
+++ Conductor.Domain/Models/Step.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using WorkflowCore.Models;
+
+namespace Conductor.Domain.Models
+{
+    public class Step
+    {
+        public string StepType { get; set; }
+
+        public string Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string CancelCondition { get; set; }
+
+        public WorkflowErrorHandling? ErrorBehavior { get; set; }
+
+        public TimeSpan? RetryInterval { get; set; }
+
+        public List<List<Step>> Do { get; set; } = new List<List<Step>>();
+
+        public List<Step> CompensateWith { get; set; } = new List<Step>();
+
+        public bool Saga { get; set; } = false;
+
+        public string NextStepId { get; set; }
+
+        public Dictionary<string, string> Inputs { get; set; } = new Dictionary<string, string>();
+
+        public Dictionary<string, string> Outputs { get; set; } = new Dictionary<string, string>();
+
+
+    }
+}
---
+++ Conductor.Domain/Models/Workflow.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using WorkflowCore.Models;
+
+namespace Conductor.Domain.Models
+{
+    public class Workflow
+    {
+        public string Id { get; set; }
+
+        public int Version { get; set; }
+
+        public string Description { get; set; }
+
+        //public string DataType { get; set; }
+
+        public WorkflowErrorHandling DefaultErrorBehavior { get; set; }
+
+        public TimeSpan? DefaultErrorRetryInterval { get; set; }
+
+        public List<Step> Steps { get; set; } = new List<Step>();
+    }
+}
