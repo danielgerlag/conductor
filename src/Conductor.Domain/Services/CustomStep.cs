0cd2939c8408b3039d50b718d7b8ebf087ad9873
Daniel Gerlag <daniel@gerlag.ca>
2019-08-03 09:42:25 -0700

    wip

---
+++ src/Conductor.Domain/Services/CustomStep.cs
@@ -0,0 +1,37 @@
+﻿using Conductor.Domain.Interfaces;
+using Conductor.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using WorkflowCore.Interface;
+using WorkflowCore.Models;
+
+namespace Conductor.Domain.Services
+{
+    public class CustomStep : StepBodyAsync
+    {
+        private ICustomStepService _service;
+
+        public Dictionary<string, object> _variables { get; set; } = new Dictionary<string, object>();
+
+        public object this[string propertyName]
+        {
+            get => _variables[propertyName];
+            set => _variables[propertyName] = value;
+        }
+
+        public CustomStep(ICustomStepService service)
+        {
+            _service = service;
+        }
+
+        public async override Task<ExecutionResult> RunAsync(IStepExecutionContext context)
+        {
+            var resource =_service.GetStepResource(Convert.ToString(_variables["__custom_step__"]));
+            
+            _service.Execute(resource, _variables);
+            return ExecutionResult.Next();
+        }
+    }
+}
