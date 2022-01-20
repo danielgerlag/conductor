bf3390abe5d2c96067b362a32f830a7c918d37ec
Razvan Predescu <rzvdaniel@gmail.com>
2022-01-20 23:36:33 +0000

    Added Flow controller

---
+++ src/Conductor/Controllers/FlowController.cs
@@ -0,0 +1,64 @@
+﻿using Conductor.Auth;
+using Conductor.Domain.Interfaces;
+using Conductor.Domain.Models;
+using Conductor.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace Conductor.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FlowController : ControllerBase
+    {
+        private readonly IFlowService _service;
+
+        public FlowController(IFlowService service)
+        {
+            _service = service;
+        }
+
+        [HttpGet]
+        [Authorize(Policy = Policies.Author)]
+        public ActionResult<IEnumerable<Flow>> Get([FromQuery] PaginationParameter parameter)
+        {
+            var result = _service.GetFlows(parameter.PageNumber, parameter.PageSize);
+
+            return Ok(result);
+        }
+
+        [HttpGet("{id}")]
+        [Authorize(Policy = Policies.Author)]
+        public ActionResult<Flow> Get(string id)
+        {
+            var result = _service.GetFlow(id);
+
+            if (result == null)
+                return NotFound();
+
+            return Ok(result);
+        }
+
+        [HttpPost]
+        [Authorize(Policy = Policies.Author)]
+        public void Post([FromBody] Flow value)
+        {
+            _service.RegisterNewFlow(value);
+            Response.StatusCode = 204;
+        }
+
+        //[HttpPut]
+        //public void Put([FromBody] string value)
+        //{
+        //    _service.RegisterNewFlow(value);
+        //}
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        [Authorize(Policy = Policies.Author)]
+        public void Delete(int id)
+        {
+        }
+    }
+}
